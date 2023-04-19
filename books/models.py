0c1c03a7ddfe9e20acf7e42cbb8e46eadcec010d
Pizzaonpluto <whateverfloatsyourboat0811@gmail.com>
2023-04-19 23:05:59 +0900

    relieve create conditions for book and chapter

--- books/models.py
+++ books/models.py
@@ -3,15 +3,16 @@ from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=1024)
-    title_prompt = models.CharField(max_length=1024)
-    title_gen = models.CharField(max_length=8192)
+    title_prompt = models.CharField(max_length=1024, blank=True)
+    title_gen = models.CharField(max_length=8192, blank=True)
     description = models.TextField()
-    description_prompt = models.TextField()
-    description_gen = models.TextField()
+    description_prompt = models.TextField(blank=True)
+    description_gen = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+#    updated_by = models.ForeignKey(User, null=True, related_name='+')
     generated_at = models.DateTimeField(null=True)
-
+    
 class Chapter(models.Model):
     title = models.CharField(max_length=1024)
     title_prompt = models.CharField(max_length=1024)
@@ -20,7 +21,9 @@ class Chapter(models.Model):
     content_prompt = models.TextField()
     content_gen = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+#    updated_by = models.ForeignKey(User, null=True, related_name='+')
     generated_at = models.DateTimeField(null=True)
     book = models.ForeignKey(Book, related_name='chapters',
                              on_delete=models.RESTRICT)
+    
